caeec7bb619a69b161cfd0d7bd5894497ccb173b
pnltoen <pnltoen@gmail.com>
2025-06-18 03:37:14 +0900

    Initial commit

---
+++ Assets/Piper.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using UnityEngine;
+using Newtonsoft.Json;
+using Unity.InferenceEngine;
+using System.Collections;
+using FF = Unity.InferenceEngine.Functional;
+using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine.UI;
+
+public class Piper : MonoBehaviour
+{
+    Dictionary<string, int> vocab = new Dictionary<string, int>();
+    Dictionary<int, string> idToToken = new Dictionary<int, string>();
+    Dictionary<string, string> arpabetToIPA = new Dictionary<string, string>();
+    Dictionary<string, int> IPAToIDs = new Dictionary<string, int>();
+
+    public TMP_InputField inputField;
+
+    Model bartEncoder;
+    Worker bartEncoder_Worker;
+
+    Model bartDecoder;
+    Worker bartDecoder_Worker;
+
+    Model piper;
+    Worker piper_Worker;
+
+    Worker argmax;
+
+    Tensor<int> inputIDs_encoder;
+    Tensor<int> attentionMask_bart;
+    Tensor<float> encoder_hidden_states;
+
+    int tokenCount = 0;
+
+    const int START_TOKEN = 0;
+    const int END_TOKEN = 2;
+    const int MAX_DECODE = 128;
+
+    const BackendType backend = BackendType.CPU;
+
+    List<string> tokens;
+    List<List<string>> punctuationSlots;
+    int[] decoderTokens;
+
+    List<int> allPhonemeTokenIDs = new List<int>();
+
+    // Set a larger number for faster GPUs
+    public int k_LayersPerFrame = 250;
+
+    IEnumerator piper_Async;
+
+    string defaultVoice = "en_US_lessac_high.sentis";
+    public float[] scales = { 0.4f, 1.1f, 0.6f }; //[noise_scale, length_scale, noise_w]
+
+    void Start()
+    {
+        LoadPretrainedData();
+        LoadBartModel();
+        LoadPiperModel(defaultVoice);
+        argMax();
+    }
+
+    public void SwitchSpeaker(Toggle toggle)
+    {
+        if (toggle.isOn)
+        {
+            string voice = toggle.gameObject.name + ".sentis";
+            LoadPiperModel(voice);
+        }
+    }
+
+    public void GenerateTTS(string _)
+    {
+        string input = inputField.text;
+        Tokenizer(input);
+        StartCoroutine(RunModel(tokens));
+    }
+
+    void LoadBartModel()
+    {
+        bartEncoder = ModelLoader.Load(Application.streamingAssetsPath + "/encoder_model.sentis");
+        bartEncoder_Worker = new Worker(bartEncoder, backend);
+
+        bartDecoder = ModelLoader.Load(Application.streamingAssetsPath + "/decoder_model.sentis");
+        bartDecoder_Worker = new Worker(bartDecoder, backend);
+    }
+
+    void LoadPiperModel(string voice)
+    {
+        string piper_Path = Path.Combine(Application.streamingAssetsPath, voice);
+        piper_Worker?.Dispose();
+        piper = ModelLoader.Load(piper_Path);
+        piper_Worker = new Worker(piper, BackendType.GPUCompute);
+    }
+
+    void LoadPretrainedData()
+    {
+        string Vocabpath = Application.streamingAssetsPath + "/g2p_vocab.json";
+        string IPApath = Application.streamingAssetsPath + "/piper_IPA.json";
+        string IPAIDspath = Application.streamingAssetsPath + "/piper_IDs.json";
+
+        // Vocab Load
+        if (!File.Exists(Vocabpath))
+        {
+            return;
+        }
+
+        try
+        {
+            string jsonText = File.ReadAllText(Vocabpath);
+            vocab = JsonConvert.DeserializeObject<Dictionary<string, int>>(jsonText);
+            idToToken = vocab.ToDictionary(kv => kv.Value, kv => kv.Key);
+        }
+        catch (Exception e)
+        {
+        }
+
+        // IPA Mapping Load
+        if (!File.Exists(IPApath))
+        {
+            return;
+        }
+
+        try
+        {
+            string ipaJson = File.ReadAllText(IPApath);
+            arpabetToIPA = JsonConvert.DeserializeObject<Dictionary<string, string>>(ipaJson);
+        }
+        catch (Exception e)
+        {
+        }
+
+        if (!File.Exists(IPAIDspath))
+        {
+            return;
+        }
+
+        try
+        {
+            string ipaIdJson = File.ReadAllText(IPAIDspath);
+            IPAToIDs = JsonConvert.DeserializeObject<Dictionary<string, int>>(ipaIdJson);
+        }
+        catch (Exception e)
+        {
+        }
+
+    }
+
+    void argMax()
+    {
+        FunctionalGraph graph = new FunctionalGraph();
+        var input = graph.AddInput(DataType.Float, new DynamicTensorShape(-1, -1, -1));
+        var amax = FF.ArgMax(input, -1, false);
+        var selectTokenModel = graph.Compile(amax);
+        argmax = new Worker(selectTokenModel, backend);
+    }
+
+    public void Tokenizer(string sentence)
+    {
+        tokens = new List<string>();
+        punctuationSlots = new List<List<string>>();
+
+        string[] raw = sentence.ToLower().TrimEnd().Split(' ');
+
+        for (int i = 0; i < raw.Length; i++)
+        {
+            string word = raw[i];
+            List<string> punctuations = new List<string>();
+
+            if (!string.IsNullOrWhiteSpace(word))
+            {
+                for (int j = word.Length - 1; j >= 0; j--)
+                {
+                    if (!char.IsLetterOrDigit(word[j]))
+                    {
+                        punctuations.Add(word[j].ToString());
+                    }
+                    else
+                    {
+                        word = word.Substring(0, j + 1);
+                        break;
+                    }
+                }
+            }
+
+            tokens.Add(word);
+            punctuationSlots.Add(punctuations);
+        }
+    }
+
+    (Tensor<float>, Tensor<int>) RunEncoder(string word)
+    {
+        string[] chars = word.Select(c => c.ToString()).ToArray();
+        int[] middle = chars.Select(t => vocab.ContainsKey(t) ? vocab[t] : vocab["<unk>"]).ToArray();
+
+        int[] inputIDs = new int[middle.Length + 2];
+        inputIDs[0] = vocab["<s>"];
+        Array.Copy(middle, 0, inputIDs, 1, middle.Length);
+        inputIDs[inputIDs.Length - 1] = vocab["</s>"];
+
+        int[] attentionMask = Enumerable.Repeat(1, inputIDs.Length).ToArray();
+
+        var inputIDs_encoder = new Tensor<int>(new TensorShape(1, inputIDs.Length), inputIDs);
+        attentionMask_bart = new Tensor<int>(new TensorShape(1, attentionMask.Length), attentionMask);
+
+        bartEncoder_Worker.Schedule(inputIDs_encoder, attentionMask_bart);
+        encoder_hidden_states = bartEncoder_Worker.PeekOutput("last_hidden_state").ReadbackAndClone() as Tensor<float>;
+        inputIDs_encoder.Dispose();
+
+        return (encoder_hidden_states, attentionMask_bart);
+    }
+
+    (int[], int) RunDecoder(Tensor<float> encoder_hidden_states, Tensor<int> attentionMask_bart)
+    {
+        decoderTokens = new int[MAX_DECODE];
+        decoderTokens[0] = END_TOKEN;
+        decoderTokens[1] = START_TOKEN;
+        tokenCount = 2;
+
+        for (int step = 0; step < MAX_DECODE; step++)
+        {
+            var decoder_input_slice = decoderTokens.Take(tokenCount).ToArray();
+            var decoder_input_tensor = new Tensor<int>(new TensorShape(1, tokenCount), decoder_input_slice);
+
+            bartDecoder_Worker.Schedule(attentionMask_bart, decoder_input_tensor, encoder_hidden_states);
+            var logits = bartDecoder_Worker.PeekOutput("logits") as Tensor<float>;
+            using var cpuLogits = logits.ReadbackAndClone() as Tensor<float>;
+            logits.Dispose();
+
+            int vocabSize = cpuLogits.shape[2];
+            float[] lastLogits = new float[vocabSize];
+            for (int i = 0; i < vocabSize; i++)
+                lastLogits[i] = cpuLogits[0, tokenCount - 1, i];
+
+            var lastLogitsTensor = new Tensor<float>(new TensorShape(1, 1, vocabSize), lastLogits);
+
+            argmax.Schedule(lastLogitsTensor);
+
+            using var t_Token = argmax.PeekOutput().ReadbackAndClone() as Tensor<int>;
+            int nextToken = t_Token[0];
+
+            decoderTokens[tokenCount] = nextToken;
+            tokenCount++;
+
+            lastLogitsTensor.Dispose();
+            decoder_input_tensor.Dispose();
+
+            if (nextToken == END_TOKEN)
+                break;
+        }
+        encoder_hidden_states.Dispose();
+        attentionMask_bart.Dispose();
+
+        return (decoderTokens, tokenCount);
+    }
+
+    public List<int> DecodeToIPAIDs(int[] decoderTokens, int tokenCount, int token_order)
+    {
+        int[] validTokenIDs = decoderTokens
+            .Take(tokenCount)
+            .Where(id => id != 0 && id != 2)
+            .ToArray();
+
+        List<string> arpabetTokens = new List<string>();
+        foreach (int id in validTokenIDs)
+        {
+            if (idToToken.TryGetValue(id, out string token))
+                arpabetTokens.Add(token);
+        }
+
+        List<string> ipaTokens = new List<string>();
+        foreach (string arp in arpabetTokens)
+        {
+            if (arpabetToIPA.TryGetValue(arp, out string ipa))
+                ipaTokens.Add(ipa);
+        }
+
+        ipaTokens.AddRange(punctuationSlots[token_order]);
+
+        foreach (string ipa in ipaTokens)
+        {
+            foreach (char c in ipa)
+            {
+                if (IPAToIDs.TryGetValue(c.ToString(), out int id))
+                {
+                    allPhonemeTokenIDs.Add(id);
+                }
+            }
+            allPhonemeTokenIDs.Add(0);
+        }
+
+        allPhonemeTokenIDs.Add(3);
+        allPhonemeTokenIDs.Add(0);
+
+        return allPhonemeTokenIDs;
+    }
+
+    IEnumerator RunPiper()
+    {
+        int count = allPhonemeTokenIDs.Count;
+        allPhonemeTokenIDs.Insert(0, 1);  // START
+        allPhonemeTokenIDs.RemoveAt(allPhonemeTokenIDs.Count - 1);
+        allPhonemeTokenIDs[allPhonemeTokenIDs.Count - 2] = 2;
+        var phonemes_Ids = allPhonemeTokenIDs.ToArray();
+        allPhonemeTokenIDs.Clear();
+
+        var piper_input = new Tensor<int>(new TensorShape(1, phonemes_Ids.Length), phonemes_Ids);
+        var piper_input_lengths = new Tensor<int>(new TensorShape(1), new int[] { phonemes_Ids.Length });
+        var piper_scales = new Tensor<float>(new TensorShape(scales.Length), scales);
+
+        piper_Async = piper_Worker.ScheduleIterable(piper_input, piper_input_lengths, piper_scales);
+
+        int it = 0;
+        while (piper_Async.MoveNext())
+        {
+            if (++it % k_LayersPerFrame == 0)
+                yield return null;
+        }
+        yield return null;
+
+        var piper_output = piper_Worker.PeekOutput("output") as Tensor<float>; // batch x Channel x Height x Time
+        var awaiter = piper_output.ReadbackAndCloneAsync().GetAwaiter();
+
+        awaiter.OnCompleted(() =>
+        {
+            var piper_CPUoutput = awaiter.GetResult().DownloadToArray();
+            PlayAndSavePiperAudio(piper_CPUoutput);
+
+            piper_output.Dispose();
+            piper_input.Dispose();
+            piper_input_lengths.Dispose();
+            piper_scales.Dispose();
+        });
+    }
+
+    IEnumerator RunModel(List<string> tokens)
+    {
+        for (int i = 0; i < tokens.Count; i++)
+        {
+            RunEncoder(tokens[i]);
+            RunDecoder(encoder_hidden_states, attentionMask_bart);
+            DecodeToIPAIDs(decoderTokens, tokenCount, i);
+
+            yield return null;
+        }
+        StartCoroutine(RunPiper());
+    }
+
+    void PlayAndSavePiperAudio(float[] output)
+    {
+        AudioClip clip = AudioClip.Create("PiperAudio", output.Length, 1, 22050, false);
+
+        clip.SetData(output, 0);
+
+        AudioSource audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+
+    void OnDestroy()
+    {
+        argmax?.Dispose();
+        inputIDs_encoder?.Dispose();
+        attentionMask_bart?.Dispose();
+        encoder_hidden_states?.Dispose();
+        bartEncoder_Worker?.Dispose();
+        bartDecoder_Worker?.Dispose();
+        piper_Worker?.Dispose();
+    }
+}
---
+++ README.md
@@ -0,0 +1,80 @@
+# unity-piper-g2p-inference-runtime
+
+[![Watch the demo](https://img.youtube.com/vi/tHMdiyMR3i8/0.jpg)](https://www.youtube.com/watch?v=tHMdiyMR3i8)
+
+## 🎙️ Project Overview
+
+This project demonstrates a **REALTIME**, fully **ON-DEVICE** Text-to-Speech (TTS) system implemented in Unity.  
+Built with **Unity Inference Engine** and optimized through **asynchronous processing**, it runs both **grapheme-to-phoneme (G2P)** conversion and **speech synthesis** locally on the device—without relying on any external APIs or internet connection.
+
+This eliminates latency, network dependency, and additional costs often associated with cloud-based solutions.  
+By integrating the **Piper TTS engine**, the system supports **multiple voices and languages**, delivering smooth and responsive audio generation even on mid-range hardware.
+
+
+---
+## 🚀 Usage
+
+1. Clone the repository:  
+   git clone https://github.com/pnltoen/unity-piper-g2p-inference-runtime.git  
+   cd unity-piper-g2p-inference-runtime  
+
+2. Download [`StreamingAssets.zip`](https://drive.google.com/file/d/17rJyCFnemh5MqJvGjNWWyLT0kOdiRCmF/view?usp=sharing) and extract it into the `Assets/` folder.  
+   Make sure the full path becomes: `Assets/StreamingAssets/`
+
+3. Open the Unity project and load the scene located at:  
+   `Assets/piper.unity`
+
+4. Download the VisionOS-style UI asset from the [Unity Asset Store](https://assetstore.unity.com/packages/tools/gui/ui-kit-for-vision-pro-os-265406)
+
+5. In the **Hierarchy**, select the `piper_engine` object and adjust the **Piper TTS scales**:  
+   - `noise_scale` (Default: `0.4`, Trained: `0.667`)  
+   - `length_scale` (Default: `1.1`, Trained: `1`)  
+   - `noise_w` (Default: `0.6`, Trained: `0.8`)
+
+6. Press **Play** in the Editor and type the sentence you want to synthesize.
+
+7. You can select different voices from the left panel.  
+   For additional voices, refer to [piper/VOICES.md](https://github.com/rhasspy/piper/blob/master/VOICES.md)
+
+
+## 🔍 Limitations and Notes
+
+- `mini-bart-g2p` operates on a **word-level basis**, so context-sensitive pronunciations (e.g., `read` in past vs. present tense, or `use` as noun vs. verb) are not accurately distinguished.
+
+- While **Piper** supports multilingual voices, this project uses `mini-bart-g2p`, which only accepts **English text** as input. Non-English text will not be converted properly to phonemes.
+
+- Each input sentence is processed **one word per frame**, where the G2P encoder-decoder runs per word.  
+  After all words are processed, the phoneme sequence is passed asynchronously to the Piper model for **real-time audio synthesis and playback**.
+
+- Although `mini-bart-g2p` can run on GPU, its **auto-regressive** structure requires downloading predicted tokens to the CPU at each step.  
+  This frequent GPU-to-CPU transfer introduces overhead, making pure CPU execution faster and more efficient in this case.
+
+- The **Piper TTS model** runs on the **GPU** to ensure smooth real-time speech synthesis.  
+  The entire inference process—from phoneme input to audio playback—is handled **asynchronously** for low-latency performance.  
+  You can fine-tune responsiveness by adjusting the `k_LayersPerFrame` variable, which controls how many decoder layers are processed per frame.
+
+## 📚 Additional Resources
+
+For more implementation details and practical usage of Unity Inference Engine with TTS and G2P, refer to the following (Korean blog posts):
+
+- [Implementing Piper TTS using Unity Inference Engine](https://pnltoen.tistory.com/entry/Unity-Inference-Engine%EC%9D%84-%ED%99%9C%EC%9A%A9%ED%95%9C-Piper-TTS-Text-To-Speech-%EA%B5%AC%ED%98%84) *(Korean blog post)*
+- [Exploring Grapheme-to-Phoneme (G2P) in Unity Inference Engine](https://pnltoen.tistory.com/entry/unity-inference-engine%EC%97%90%EC%84%9C-g2p-grapheme-to-phoneme-%EA%B3%A0%EC%B0%B0) *(Korean blog post)*
+- [Preprocessing with Python Server in Unity Inference Engine](https://pnltoen.tistory.com/entry/%EC%9C%A0%EB%8B%88%ED%8B%B0%EC%97%90%EC%84%9C-Python-%EC%84%9C%EB%B2%84-%ED%99%9C%EC%9A%A9%ED%95%B4-Unity-Inference-Engine-%EC%A0%84%EC%B2%98%EB%A6%AC-%ED%95%B4%EB%B3%B4%EA%B8%B0) *(Korean blog post)*
+
+---
+## 📄 License
+
+This project (`unity-piper-g2p-inference-runtime`) is released under the **Apache License 2.0**.
+
+However, it includes third-party machine learning models and phonemization tools that are governed by their own licenses:
+
+### ▶️ Included Models and Licenses
+
+- [mini-bart-g2p (Cisco AI)](https://huggingface.co/cisco-ai/mini-bart-g2p) – **Apache License 2.0**
+- [Piper TTS (Rhasspy)](https://github.com/rhasspy/piper) – **MIT License**
+
+
+All external models used in this project are serialized and stored in `Assets/StreamingAssets/`.  
+Their original license texts are also included in that directory.
+
+> ⚠️ Please review and comply with each license when redistributing or modifying any part of this project that includes third-party components.
